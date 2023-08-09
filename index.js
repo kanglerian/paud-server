09938e93448aebf8bc7ea4f2c0600f92ca4b8d28
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-08-09 16:31:39 +0700

    cek

--- index.js
+++ index.js
@@ -5,25 +5,39 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
-const verifyToken = require('./middleware/verifyToken');
+// const verifyToken = require('./middleware/verifyToken');
 const { User } = require('./models');
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
 
 const generateAccessToken = (user) => {
-  return jwt.sign({ id: user.id, username: user.username, email: user.email }, ACCESS_TOKEN_SECRET, { expiresIn: '50s' });
+  return jwt.sign({ id: user.id, username: user.username, email: user.email }, ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
 }
 
 const generateRefreshToken = (user) => {
@@ -40,52 +54,69 @@ app.get('/users', async (req, res) => {
 })
 
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({
-      where: {
-        username: username
-      }
-    });
-    if (!user) return res.status(400).json({ msg: 'Akun tidak ditemukan' });
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(400).json({ msg: 'Password salah' });
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
-    await User.update({ refresh_token: refreshToken }, {
-      where: {
-        id: user.id
-      }
-    })
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000
-    });
-    res.json({ accessToken });
-})
-
-app.get('/protected', verifyToken, (req, res) => {
-  res.send('oke');
+  const { username, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      username: username
+    }
+  });
+  if (!user) return res.status(400).json({ msg: 'Akun tidak ditemukan' });
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ msg: 'Password salah' });
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+  await User.update({ refresh_token: refreshToken }, {
+    where: {
+      id: user.id
+    }
+  })
+  res.json({ accessToken, refreshToken });
 });
 
-app.get('/token', async (req, res) => {
-  try {
-    const refreshToken = req.cookies.refreshToken;
-    if (!refreshToken) return res.sendStatus(401);
-    const user = await User.findOne({
-      where: {
-        refresh_token: refreshToken
-      }
-    });
-    if (!user) return res.sendStatus(403);
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-      if (err) return res.sendStatus(403);
-      const accessToken = jwt.sign({ id: user.id, username: user.username, email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '50s' });
-      res.json({ accessToken });
-    });
-  } catch (error) {
-    res.send(error)
+app.get('/set-cookie', (req, res) => {
+  res.cookie('username', 'john_doe');
+  res.send('Cookie username telah diatur');
+});
+
+app.get('/get-cookie', (req, res) => {
+  const username = req.cookies.username;
+  if (username) {
+    res.send(`Nilai cookie username: ${username}`);
+  } else {
+    res.send('Cookie username tidak ditemukan');
   }
-})
+});
+
+app.get('/protected', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    res.json({ message: 'Access granted', user: decoded });
+  });
+});
+
+app.post('/refresh', async (req, res) => {
+  const { refreshToken } = req.cookies;
+  console.log(refreshToken);
+  // const user = await User.findOne({
+  //   where: {
+  //     refresh_token: refreshToken
+  //   }
+  // });
+  // if (!refreshToken || !user) {
+  //   return res.sendStatus(401);
+  // }
+
+  // jwt.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, user) => {
+  //   if (err) {
+  //     return res.sendStatus(403);
+  //   }
+  //   const accessToken = jwt.sign({ id: user.id, username: user.username }, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+  //   res.json({ accessToken });
+  // });
+});
 
 app.delete('/logout', async (req, res) => {
 
@@ -100,7 +131,7 @@ app.delete('/logout', async (req, res) => {
   const userId = user.id
   await User.update({
     refresh_token: null
-  },{
+  }, {
     where: {
       id: userId
     }
