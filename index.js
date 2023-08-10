1ad64befede2f67658694ec65e130f3db44db86a
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-08-10 11:48:54 +0700

    tambah origins

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const corsOptions = {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     // Allow requests from specific origins
-    const allowedOrigins = ['https://paud-client.vercel.app'];
+    const allowedOrigins = ['https://paud-client.vercel.app','https://api.politekniklp3i-tasikmalaya.ac.id/'];
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
